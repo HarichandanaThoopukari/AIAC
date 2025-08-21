110416543df1f46ff619d6fbacebf905bd71273b
HarichandanaThoopukari <thoopukariharichandana@gmail.com>
2025-08-21 10:49:18 +0530

    Delete lab4 directory

--- lab4/task2copilot.py
+++
@@ -1,6 +0,0 @@
-def cm_to_inches(cm):
-    return cm / 2.54
-
-cm = float(input("Enter length in centimeters: "))
-inches = cm_to_inches(cm)
-print(f"{cm} centimeters is equal to {inches} inches.")
--- lab4/task5copilot.py
+++
@@ -1,7 +0,0 @@
-def count_lines_in_file(filename):
-    with open(filename, 'r', encoding='utf-8') as f:
-        return len(f.readlines())
-
-if __name__ == "__main__":
-    num_lines = count_lines_in_file('a.txt')
-    print(f"Number of lines in a.txt: {num_lines}")
